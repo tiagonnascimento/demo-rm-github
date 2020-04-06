87b69478d3f08183618fd8c03bb4de49f895ecec
Tiago Nascimento <tnascimento@salesforce.com>
2020-04-05 23:30:37 -0300

    creating onchange handler

--- force-app/main/default/lwc/riskAssessmentFlow/riskAssessmentFlow.js
+++ force-app/main/default/lwc/riskAssessmentFlow/riskAssessmentFlow.js
@@ -3,4 +3,24 @@ import getRiskAssessmentQuestionaire from '@salesforce/apex/RiskAssessmentFlowCo
 
 export default class RiskAssessmentFlow extends LightningElement {
     @wire(getRiskAssessmentQuestionaire) sections;
+
+    respostas = new Map();
+    score = new Map();
+
+    handleChange(evt) {
+        var selectedResponse;
+        if (evt.target.type == 'button') {
+            this.score.set(evt.target.label, evt.target.value);
+            for (let i = 0; i < evt.target.options.length; i++) {
+                if (evt.target.options[i].value == evt.target.value) {
+                    selectedResponse = evt.target.options[i].label;
+                    break;
+                }
+            }
+        } else {
+            selectedResponse = evt.target.value; 
+        }
+
+        this.respostas.set(evt.target.label, selectedResponse);
+    }
 }
